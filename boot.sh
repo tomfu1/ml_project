830da060be60033d25b41ea1410a017d9f917537
Thomas Drablos <tomdrabs@thomass-air.attlocal.net>
2024-05-04 15:46:20 -0500

    bugfix: boot.sh uses specific url for torch-* libs

--- boot.sh
+++ boot.sh
@@ -4,7 +4,7 @@ set -e
 
 python3 -m venv env
 env/bin/pip install --upgrade pip
-env/bin/pip install torch wheel setuptools
-env/bin/pip install torch-sparse torch-scatter torch-cluster
+env/bin/pip install torch==2.3.0 wheel setuptools
+env/bin/pip install torch-sparse torch-scatter torch-cluster -f https://data.pyg.org/whl/torch-2.3.0+cpu.html
 env/bin/pip install git+https://github.com/tomfu1/nablaDFT.git@hack/manifest
 env/bin/pip install scikit-learn numpy PyYAML boto3
