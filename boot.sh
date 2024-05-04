cbfc3c5d4a349183f57accffbc0293ed3c3b4991
Thomas Drablos <tomdrabs@thomass-air.attlocal.net>
2024-05-04 14:57:26 -0500

    bugfix: add setuptools to boot.sh

--- boot.sh
+++ boot.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+set -e
+
 python3 -m venv env
 env/bin/pip install --upgrade pip
-env/bin/pip install torch wheel
+env/bin/pip install torch wheel setuptools
 env/bin/pip install torch-sparse torch-scatter torch-cluster
 env/bin/pip install git+https://github.com/tomfu1/nablaDFT.git@hack/manifest
 env/bin/pip install scikit-learn numpy PyYAML boto3
