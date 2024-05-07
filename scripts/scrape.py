ae934c222508eb58378bc20c2c41289bddefcc78
Thomas Drablos <tomdrabs@thomass-air.attlocal.net>
2024-05-07 10:47:59 -0500

    feature: add {hyperparameter}_vs_loss charts to scripts/scrape.py

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -3,7 +3,11 @@
 import glob
 import json
 import os
+import pandas as pd
+import seaborn as sns
 import yaml
+
+os.makedirs('results', exist_ok=True)
 
 KEYS = [
     'batch_size',
@@ -39,7 +43,12 @@ for fname in glob.iglob(os.path.join('configurations', '**', '*.json')):
     data.append({ k: get(row, k) for k in KEYS })
 data.sort(key=lambda x: x['loss'])
 
-with open('results.csv', 'w') as out:
+with open(os.path.join('results', 'results.csv'), 'w') as out:
     out.write('batch size,clip,decoder fc1 size,decoder fc2 size,encoder fc1 size,latent dimensions,leaky relu alpha,learning rate,num_epochs,reconstruction,kl divergence,minimum loss,loss')
     for row in data:
         out.write('\n' + ','.join(str(row[k]) for k in KEYS))
+
+df = pd.DataFrame.from_records(data)
+for k in ['decoder_fc1_size', 'decoder_fc2_size', 'encoder_fc1_size', 'latent_dim', 'leaky_relu_alpha', 'learning_rate', 'num_epochs']:
+    p = sns.relplot(data=df, x=k, y='loss')
+    p.savefig(os.path.join('results', f'{k}_vs_loss'))
