1e57f3cb67d52e4df82b3f70434d73f8a6d33d5a
Thomas Drablos <tomdrabs@thomass-air.attlocal.net>
2024-05-07 16:39:20 -0500

    feature: poll.py for windows

--- scripts/poll.py
+++ scripts/poll.py
@@ -7,18 +7,28 @@ import subprocess
 import pandas as pd
 import seaborn as sns
 
+os.makedirs('results', exist_ok=True)
+
+if os.name == 'nt':
+    args = ['env/Scripts/python.exe main.py -v -j']
+else:
+    args = [['env/bin/python', 'main.py', '-v', '-j']]
+
 output = subprocess.run(
-    ['python', 'main.py'],
+    *args,
     check=True,
     stderr=subprocess.PIPE,
+    stdout=subprocess.PIPE,
     text=True,
 )
 
 data = []
-for i, m in enumerate(re.finditer(r'INFO:root:Epoch \d+:\s+Average Loss:\s+(?P<loss>\d+\.\d+)', output.stderr)):
-    data.append({ 'epoch': i + 1, 'loss': float(m.group('loss')) })
+for i, m in enumerate(re.finditer(r'DEBUG:root:Batch \d+ - \d+:\s+Loss:\s+(?P<loss>\d+\.\d+)', output.stderr)):
+    data.append({ 'batch': i + 1, 'loss': float(m.group('loss')) })
 
 df = pd.DataFrame.from_records(data)
-df['epoch'] = df['epoch'].astype(str)
-p = sns.relplot(df, x='epoch', y='loss', kind='line')
+df['batch'] = df['batch'].astype(str)
+p = sns.relplot(df, x='batch', y='loss', kind='line')
 p.savefig(os.path.join('results', 'run.jpg'))
+with open(os.path.join('results', 'run.json'), 'w') as f:
+    out.write(output.stdout)
